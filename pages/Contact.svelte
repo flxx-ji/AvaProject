0dd21d603fe6bd7fc83feaf5d76cabd9abf4cce5
jean-marcbastareaud <flxxance@gmail.com>
2024-06-24 00:15:41 +0200

    refonte contactForm et importation dans contact.svelte, mise a jour du routage

--- pages/Contact.svelte
+++ pages/Contact.svelte
@@ -1,64 +1,17 @@
 <script>
-    // import {Link} from 'svelte-routing';
-    
-     let name = '';
-     let email = '';
-     let phone = '';
-     let message = '';
-
-     const submitContact = () => {
-        const formIntel = {
-          name,
-          email,
-          phone,
-          message
-        };
-
-
-     };
-     
+    import ContacForm from "../src/components/contacForm.svelte";
 </script>
-<main>
-        <div class="form">
-            <ul>
-                <li><a href="/homepage"> back to home</a></li>
-            </ul>
 
-        </div>
+<main>
+     
+    <ContacForm/>
 </main>
+
 <style>
-    .form {
-        border: 1px solid blue;
-         height: 500px;
-         width: 500px;
-    }
     main {
-        background-image: url('/image/bedroom-6686058_1280.jpg');
-        background-size: cover;
-        background-repeat: no-repeat;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
     }
 </style>
-
-<form on:submit|preventDefault={submitContact}>
-    
-<label>
-    Name:
-    <input type= "text" bind:value={name} required />
-</label>
-
-<label>
-    Email:
-    <input type="email" bind:value={email} required />
-</label>
-
-<label>
-    Phone:
-    <input type="text" bind:value={phone} required />
-</label>
-
-<label>
-    Message:
-     <textarea bind:value={message} required></textarea>
-</label>
-
-</form>
---
+++ src/components/contacForm.svelte
@@ -0,0 +1,121 @@
+<script>
+    // import {Link} from 'svelte-routing';
+     import { onMount } from 'svelte';
+     import axios from 'axios';
+
+     let name = '';
+     let email = '';
+     let phone = '';
+     let message = '';
+     let successMessage = '';
+     let errorMessage = '';
+
+     function clearForm()  {
+        name = '';
+        email = '';
+        phone = '';
+        message = '';
+     }
+
+     async function submitContact(event) {
+        event.preventDefault();
+
+        successMessage = '';
+        errorMessage = '';
+
+        try {
+            const response = await axios.post('/api/contacts', { name, email, phone, message});
+            if (response.status === 201) {
+                successMessage = 'Contact information saves successfully';
+                clearForm();
+            } else {
+                throw new Error('Failes to save contact information');
+            }
+        } catch (error) {
+            errorMessage = 'An error occured while submitting the form';
+        }
+     }
+ </script>
+
+<main>
+     <h1>Contactez nous </h1>
+
+     <form on:submit|preventDefault={submitContact}>
+
+        <div>
+            <label>
+                Name:
+                <input type= "text" bind:value={name} required />
+            </label>
+        </div>
+        
+       <div>
+        <label>
+            Email:
+            <input type="email" bind:value={email} required />
+        </label>
+       </div>
+        
+      <div>
+        <label>
+            Phone:
+            <input type="text" bind:value={phone} required />
+        </label>
+      </div>
+        
+       <div>
+        <label>
+            Message:
+             <textarea bind:value={message} required></textarea>
+        </label>
+       </div>
+
+       <button typre="submit">Submit</button>
+
+    </form>
+
+    {#if successMessage}
+       <p class="success">{successMessage}</p>
+    {/if}
+    {#if errorMessage}
+       <p classs="error">{errorMessage}</p>
+    {/if}       
+        <!-- <div class="form">
+            <ul>
+                <li><a href="/homepage"> back to home</a></li>
+            </ul>
+
+        </div> -->
+</main>
+<style>
+    .form {
+        align-items: center;
+        border: 1px solid blue;
+        display: flex;
+        flex-direction: column;
+         height: 500px;
+         width: 500px;
+    }
+    main {
+        background-image: url('/image/bedroom-6686058_1280.jpg');
+        background-size: cover;
+        background-repeat: no-repeat;
+        padding: 2rem;
+        text-align:center;
+    }
+
+    div {
+        margin-bottom: 1rem;
+    }
+
+    success {
+        color:green;
+    }
+
+    error {
+        color: red;
+    }
+</style>
+
+
+ 
